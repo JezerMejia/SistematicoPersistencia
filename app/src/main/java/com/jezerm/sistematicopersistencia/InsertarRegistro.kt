94623dae26354c0a9e70c4396177e6284f31016c
JezerM <jezer.mejia13523@est.uca.edu.ni>
2023-05-11 14:05:38 -0600

    Last commit by Jezer Mejía 00031725

--- app/src/main/java/com/jezerm/sistematicopersistencia/InsertarRegistro.kt
+++ app/src/main/java/com/jezerm/sistematicopersistencia/InsertarRegistro.kt
@@ -1,14 +1,17 @@
 package com.jezerm.sistematicopersistencia
 
+import android.graphics.fonts.FontStyle
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jezerm.sistematicopersistencia.database.Repositorio
 import com.jezerm.sistematicopersistencia.database.entities.EntityProducto
 import com.jezerm.sistematicopersistencia.ui.theme.SistematicoPersistenciaTheme
@@ -41,6 +44,8 @@ fun InsertarRegistro() {
                 .padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            Spacer(modifier = Modifier.height(32.dp))
+            Text("Insertar producto", fontSize = 24.sp, fontWeight = FontWeight.Bold)
             Spacer(modifier = Modifier.height(32.dp))
             OutlinedTextField(value = nombre, onValueChange = { nombre = it }, label = {
                 Text(text = "Nombre")
